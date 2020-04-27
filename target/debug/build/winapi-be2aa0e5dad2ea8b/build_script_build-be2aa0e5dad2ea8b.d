193092217d87abb139f6353cf208eec89fe2645d
meltedot <meltedalt@gmail.com>
2020-04-27 22:52:17 +0100

    del

--- target/debug/build/winapi-be2aa0e5dad2ea8b/build_script_build-be2aa0e5dad2ea8b.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Poliveira\Desktop\DEV\Rust\utile_cli\target\debug\build\winapi-be2aa0e5dad2ea8b\build_script_build-be2aa0e5dad2ea8b.exe: C:\Users\Poliveira\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs
-
-c:\Users\Poliveira\Desktop\DEV\Rust\utile_cli\target\debug\build\winapi-be2aa0e5dad2ea8b\build_script_build-be2aa0e5dad2ea8b.d: C:\Users\Poliveira\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs
-
-C:\Users\Poliveira\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs:
